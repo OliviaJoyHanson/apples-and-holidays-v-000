d0fa7534a88e5351c73bf6542cf7e5b5ee0000e2
Olivia Hanson <olivia.joy.hanson@gmail.com>
2020-05-17 14:28:08 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -79,7 +79,7 @@ def all_holidays_with_bbq(holiday_hash)
   hash = holiday_hash.select{|season, holidays| holidays.values.flatten.include?("BBQ")}
   hash_values = hash.values
   holidays = []
-  binding.pry
+  #binding.pry
   hash_values.each{|hash| hash.each{|holiday, supplies_array| holidays << holiday}}
   holidays
 end
